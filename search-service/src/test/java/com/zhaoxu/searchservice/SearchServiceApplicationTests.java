333d59656e786b11b67152aed2ed085cdc3f7515
zhaoxu <zhaoxukl1314@163.com>
2020-01-02 21:43:24 +0800

    init

--- search-service/src/test/java/com/zhaoxu/searchservice/SearchServiceApplicationTests.java
+++ search-service/src/test/java/com/zhaoxu/searchservice/SearchServiceApplicationTests.java
@@ -24,7 +24,24 @@ class SearchServiceApplicationTests {
     @Test
     void contextLoads() throws IOException {
 
+        // 查询mysql数据
         List<PmsSkuInfo> pmsSkuInfoList = new ArrayList<>();
+
+        pmsSkuInfoList = skuService.getAllSku("61");
+
+        // 转化为es的数据结构
+        List<PmsSearchSkuInfo> pmsSearchSkuInfos = new ArrayList<>();
+
+        for (PmsSkuInfo pmsSkuInfo : pmsSkuInfoList) {
+            PmsSearchSkuInfo pmsSearchSkuInfo = new PmsSearchSkuInfo();
+
+            BeanUtils.copyProperties(pmsSkuInfo,pmsSearchSkuInfo);
+
+            pmsSearchSkuInfo.setId(Long.parseLong(pmsSkuInfo.getId()));
+
+            pmsSearchSkuInfos.add(pmsSearchSkuInfo);
+
+        }
     }
 
 }
