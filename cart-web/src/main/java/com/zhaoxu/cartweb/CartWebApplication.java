da0c1be8f6b39166d35dd5608ce658a86a36a74e
zhaoxukl1314 <zhaoxuk13kl>
2020-02-13 09:07:28 +0800

    initialize

---
+++ cart-web/src/main/java/com/zhaoxu/cartweb/CartWebApplication.java
@@ -0,0 +1,13 @@
+package com.zhaoxu.cartweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartWebApplication.class, args);
+    }
+
+}
---
+++ cart-web/src/test/java/com/zhaoxu/cartweb/CartWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoxu.cartweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
